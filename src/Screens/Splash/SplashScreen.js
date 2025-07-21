8f599e2b1cc408ecf43855db058cb20997529776
KAWAKI0101 <hv3346433@gmail.com>
2025-07-21 08:46:24 +0530

    Initial commit

--- src/Screens/Splash/SplashScreen.js
+++ src/Screens/Splash/SplashScreen.js
@@ -1,43 +1,34 @@
-// src/screens/Splash/SplashScreen.js
-
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Colors, Fonts } from '../../utils/Constants';
-import LinearGradient from 'react-native-linear-gradient'; // 👈 Import this
+import { StyleSheet, Image, View, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 export default function SplashScreen({ navigation }) {
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       navigation.replace('Intro');
     }, 2000);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
-    <LinearGradient
-      colors={[Colors.primary, Colors.primary_light, Colors.secondary]} // 👈 Use your two theme colors here
-      style={styles.container}
-    >
-      <Text style={styles.logo}>FLOT</Text>
-      <Text style={styles.tagline}>Flexible Loan on Time</Text>
-    </LinearGradient>
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/Image/splash.png')}
+        style={styles.image}
+        resizeMode="cover"
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-  logo: {
-    fontSize: 40,
-    fontFamily: Fonts.Bold,
-    color: '#fff',
-  },
-  tagline: {
-    marginTop: 10,
-    fontSize: 16,
-    fontFamily: Fonts.Medium,
-    color: '#fff',
+  image: {
+    width: width,
+    height: height,
   },
 });
